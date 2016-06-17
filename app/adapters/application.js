938eaf20d1cbaefed100d11c5b4ebe93d3c59b23
Benny Thomä <bennythomae@bennys-mbp.speedport.ip>
2016-06-17 16:36:00 +0200

    minor changes to the ping route

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-	host: 'http://api.afeefa.fx',
-	namespace: 'api/v1	'
+	host: 'http://localhost:3000',
+	namespace: 'api/v1'
 });
