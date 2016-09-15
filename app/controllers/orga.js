24c23e0d6b84f1e53e07fc917f18680b08d005b4
friedrichweise <hi@weise.io>
2016-09-15 16:55:49 +0200

    delete orga: output error message

--- app/controllers/orga.js
+++ app/controllers/orga.js
@@ -1,10 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  errorMessage: false,
   actions: {
     delete: function() {
       let orga = this.get('model');
-      orga.destroyRecord();
+      orga.destroyRecord().then(() => {
+        //@todo: transistion to /orgas
+        console.log("Success");
+      }, (err) => {
+        this.set('errorMessage', err.message);
+      });
     }
   }
 });
--- app/routes/orga.js
+++ app/routes/orga.js
@@ -4,6 +4,11 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
 	model(params) {
 		//@question better to make this a subroute of /orgas and simply filter the model
-    	return this.get('store').findRecord('orga', params.orga_id);
-  	}
+    return this.get('store').findRecord('orga', params.orga_id);
+  },
+  //@question: easier way to reset controller errorMessage
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.set('errorMessage', false);
+  }
 });
