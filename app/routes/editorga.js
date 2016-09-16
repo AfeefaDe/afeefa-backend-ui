cb14ef10ebb21b4b02067044f19a76177921e447
friedrichweise <hi@weise.io>
2016-09-16 14:02:41 +0200

    add cached title for editorga form

--- app/routes/editorga.js
+++ app/routes/editorga.js
@@ -4,5 +4,5 @@ export default Ember.Route.extend({
   model(params) {
   //@question better to make this a subroute of /orgas and simply filter the model
     return this.get('store').findRecord('orga', params.orga_id);
-  }
+  },
 });
