12ca203c171eaa6f1789e17536b2caea07c5fc01
Felix <fische@courages.net>
2016-10-06 17:42:45 +0200

    styling navigation and dashboard

--- app/components/afeefa-navigation.js
+++ app/components/afeefa-navigation.js
@@ -3,8 +3,17 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	session: Ember.inject.service('session'),
 	actions: {
+		toggleMenu: function() {
+      this.set('menuVisible', !this.get('menuVisible'));
+      console.debug('toggle menu', this.get('menuVisible'));
+    },
+
 		invalidateSession() {
 			this.get('session').invalidate();
 		}
-	}
+	},
+
+	title: 'Dashboard',
+	menuVisible: false
+
 });
