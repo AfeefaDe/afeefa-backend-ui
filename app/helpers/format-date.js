ccdc9f3981c027c0d69f9be006cdc0d3db6074d2
friedrichweise <hi@weise.io>
2016-10-12 14:56:27 +0200

    add comments

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 let timeagoInstance = new timeago();
 
+/*
+ * handlebar helper to output date using timeago.js
+ */
 export function formatDate(params) {
   if(params && params[0]) {
     let date = params[0];
