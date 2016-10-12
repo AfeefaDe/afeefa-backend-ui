0e2bc337fa883a703115a75af7f5d85fae9970d7
friedrichweise <hi@weise.io>
2016-10-12 14:54:28 +0200

    change serializer for alle models

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,3 +1,36 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPISerializer.extend();
+export default DS.JSONAPISerializer.extend({
+  serialize (snapshot) {
+    let json = this._super(...arguments),
+        changedAttributes = snapshot.changedAttributes(),
+        attributes = {};
+
+    Object.keys(changedAttributes).forEach(key => {
+        attributes[this.keyForAttribute(key)] = changedAttributes[key][1];
+    });
+
+    json["data"]["attributes"] = attributes;
+    //example: remove relationship from json:
+    //delete json["data"]["relationships"];
+    console.log("[custom orga serializer] serialized JSON: ", json);
+    return json;
+  },
+  payloadKeyFromModelName(modelName) {
+    //return singular model name: 'orga', 'event'
+    return modelName;
+  },
+  keyForRelationship: function(key, relationship, method) {
+    //return underscore keys for relationships
+    return Ember.String.underscore(key);
+  },
+  keyForAttribute: function(key, relationship, method) {
+    console.log(`Convert key: ${key} to ${Ember.String.underscore(key)}`);
+    //return underscore keys for attributes
+    return Ember.String.underscore(key);
+  },
+  keyForLink: function(key, relationship, method) {
+    //return underscore keys for links
+    return Ember.String.underscore(key);
+  }
+});
