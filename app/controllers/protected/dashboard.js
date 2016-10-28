5b05e0f7a000f1822d9d440ad8bdba9e32188ff1
friedrichweise <hi@weise.io>
2016-10-28 16:49:20 +0200

    implement example action to test todos

--- app/controllers/protected/dashboard.js
+++ app/controllers/protected/dashboard.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service('session')
+  session: Ember.inject.service('session'),
+  actions: {
+    test: function() {
+      //exmaple todo action
+      let todo = this.store.peekRecord('todo', 1);
+
+      todo.get('orgas').then((orgas) => {
+        console.log(orgas);
+      });
+    }
+  }
 });
