bb01677959e4052a6257626d76513f6631e8ec30
friedrichweise <hi@weise.io>
2016-10-30 22:03:55 +0100

    update models and move attributes to entry model

--- app/models/orga.js
+++ app/models/orga.js
@@ -2,14 +2,6 @@ import DS from 'ember-data';
 import entry from './entry';
 
 export default entry.extend({
-  title:          DS.attr('string'),
-  state:          DS.attr('string'),
-  stateTransition:DS.attr('string'),
-  description:    DS.attr('string', {defaultValue: 'Description...'}),
-  createdAt:      DS.attr('date'),
-  updatedAt:      DS.attr('date'),
-  stateChangedAt: DS.attr('date'),
   parentOrga:     DS.belongsTo('orga', {inverse: 'subOrgas'}),
-  subOrgas:       DS.hasMany('orga', {inverse: 'parentOrga'}),
-  category:       DS.attr('string')
+  subOrgas:       DS.hasMany('orga', {inverse: 'parentOrga'})
 });
