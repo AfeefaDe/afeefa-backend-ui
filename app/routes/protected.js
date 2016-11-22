f56628b75cab7c1ea50e8c945c673ded924cec5a
Jens Struwe <jens@struwe.net>
2016-11-22 14:04:04 +0100

    Revert "limit dashboard orgas to 5"
    Random Spielerei undo
    
    This reverts commit 2a323b4bf3dad26613dbb0de3f71913a3403dee8.

--- app/routes/protected.js
+++ app/routes/protected.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     return RSVP.hash({
       user:   this.store.findRecord('user', this.get('session.currentUser')),
       events: this.store.findAll('event', {include: 'annotations'}),
-      orgas: this.store.query('orga', {include: 'annotations', page: {offset: '0', limit: '5'}}),
+      orgas: this.store.findAll('orga', {include: 'annotations'}),
       todos: this.store.query('entry', {filter: {todo: 'all'}}),
       /*limit entry lists on dashboard*/
       listLimit: 5
