c374e7e863ca1297e2f3054263d9ad9bfa26e5c2
friedrichweise <hi@weise.io>
2016-11-23 15:09:28 +0100

    improve search behavior and fix materialize input bug

--- app/controllers/protected/search.js
+++ app/controllers/protected/search.js
@@ -6,8 +6,11 @@ export default Ember.Controller.extend({
     search: function() {
       let searchTerm = this.get('term');
       this.set('status', 'Loading...');
-      this.get('store').query('entry', {filter: {title: searchTerm}}).then((result)=> {
-        this.set('status', null);
+      this.get('store').query('entry', {filter: {title: searchTerm}, include: 'annotations'}).then((result)=> {
+        const length = result.get('length');
+        if(length===0) this.set('status', '0 Ergebnisse');
+        else this.set('status', null);
+
         this.set('searchResult', result);
       });
     }
