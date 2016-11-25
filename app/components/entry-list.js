f63d1116aac3d062a248694e2990b44d1538a1dd
friedrichweise <hi@weise.io>
2016-11-25 13:06:25 +0100

    add sorting option to entry-list and improve sorting on the dashboard

--- app/components/entry-list.js
+++ app/components/entry-list.js
@@ -1,3 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend();
+export default Ember.Component.extend({
+  /*default sort order*/
+  sortOrder: 'title',
+  instances: null,
+  sortedInstances: Ember.computed.sort('instances', 'sortDefinition'),
+  sortDefinition: Ember.computed('sortOrder', function() {
+    return [ this.get('sortOrder') ];
+  }),
+});
