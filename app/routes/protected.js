dbe5274d6388993bc3edcd51728e32ae087f7484
friedrichweise <hi@weise.io>
2016-11-25 12:48:33 +0100

    query entries with default sort order

--- app/routes/protected.js
+++ app/routes/protected.js
@@ -6,11 +6,8 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model() {
     return RSVP.hash({
       user:   this.store.findRecord('user', this.get('session.currentUser')),
-      events: this.store.findAll('event', {include: 'annotations'}),
-      orgas: this.store.findAll('orga', {include: 'annotations'}),
-      todos: this.store.query('entry', {filter: {todo: 'all'}}),
-      /*limit entry lists on dashboard*/
-      listLimit: 5
+      events: this.store.query('event', {include: 'annotations', sort:'title'}),
+      orgas: this.store.query('orga', {include: 'annotations', sort:'title'}),
     });
   },
   actions: {
--- app/routes/protected/dashboard.js
+++ app/routes/protected/dashboard.js
@@ -1,3 +1,14 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
-export default Ember.Route.extend();
+export default Ember.Route.extend({
+  model() {
+    return RSVP.hash({
+      events: this.store.peekAll('event', {include: 'annotations'}),
+      orgas: this.store.peekAll('orga', {include: 'annotations'}),
+      todos: this.store.query('entry', {filter: {todo: 'all'}}),
+      /*limit entry lists on dashboard*/
+      listLimit: 5
+    });
+  }
+});
