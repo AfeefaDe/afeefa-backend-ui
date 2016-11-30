5f5fa1594b83f7f2efe963ebfaacee7bda79de60
friedrichweise <hi@weise.io>
2016-11-30 16:09:37 +0100

    show selected category for edit mode

--- app/components/forms/categories-form.js
+++ app/components/forms/categories-form.js
@@ -3,15 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   possibleCategories: ['jobs', 'donation', 'leisure', 'language', 'community', 'general', 'medic', 'consultation'],
   instance: '',
-  selectedCategory: false,
   actions: {
     selectCategory: function(categoryId) {
       let newCategory;
       if(categoryId && this.get('possibleCategories')[categoryId]) newCategory = this.get('possibleCategories')[categoryId];
-      else newCategory = null;
       this.set('instance', newCategory);
-      //when provided we call the onChange function in the parent
-      if(this.get('onChange')) this.get('onChange')(newCategory);
     }
   }
 });
