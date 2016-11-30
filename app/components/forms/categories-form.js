52f1cc10a2b4e91a358b0cdea0967ddd27c557b3
friedrichweise <hi@weise.io>
2016-11-30 16:49:07 +0100

    try to fix default category bug - failed

--- app/components/forms/categories-form.js
+++ app/components/forms/categories-form.js
@@ -2,11 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   possibleCategories: ['jobs', 'donation', 'leisure', 'language', 'community', 'general', 'medic', 'consultation'],
-  instance: '',
+  didReceiveAttrs() {
+    this._super(...arguments);
+    /*set default category; should be selected in hbs too!*/
+    if(!this.get('instance')) this.send('selectCategory', 0);
+  },
   actions: {
     selectCategory: function(categoryId) {
       let newCategory;
-      if(categoryId && this.get('possibleCategories')[categoryId]) newCategory = this.get('possibleCategories')[categoryId];
+      if(this.get('possibleCategories')[categoryId]) newCategory = this.get('possibleCategories')[categoryId];
       this.set('instance', newCategory);
     }
   }
