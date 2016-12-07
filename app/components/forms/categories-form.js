de0685fccd7048aba74d06e2837376de30a26afc
friedrichweise <hi@weise.io>
2016-12-07 12:01:29 +0100

    change default category behavior; fix #49

--- app/components/forms/categories-form.js
+++ app/components/forms/categories-form.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   possibleCategories: ['jobs', 'donation', 'leisure', 'language', 'community', 'general', 'medic', 'consultation'],
-  didReceiveAttrs() {
-    this._super(...arguments);
-    /*set default category; should be selected in hbs too!*/
-    if(!this.get('instance')) this.send('selectCategory', 0);
-  },
   actions: {
     selectCategory: function(categoryId) {
       let newCategory;
