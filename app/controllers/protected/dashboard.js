5a74e9bee5e5fcfc81a306e895096b09cf0e4a02
friedrichweise <hi@weise.io>
2016-12-09 13:03:35 +0100

    remove example showAlert action from dashboard

--- app/controllers/protected/dashboard.js
+++ app/controllers/protected/dashboard.js
@@ -1,10 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  actions: {
-    showAlert: function() {
-      const alertData = {title: 'Hello World', description: 'A long text that descripes the error quite well and ist longer than one', isError: true};
-      this.EventBus.publish('showAlert', alertData);
-    }
-  }
-});
+export default Ember.Controller.extend();
