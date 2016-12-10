75a52e6cdbc1404b253e5c62a9c4b30b489d5049
friedrichweise <hi@weise.io>
2016-12-10 18:27:31 +0100

    #39 output alert depending on crud action

--- app/routes/protected/newevent.js
+++ app/routes/protected/newevent.js
@@ -19,7 +19,7 @@ export default Ember.Route.extend(RouteHelper, {
     willTransition(transition) {
       const controller = this.controllerFor('protected.newevent');
       //check for unsaved changes:
-      const entryChanges = hasUnsavedAttributes(controller.get('model.entryInstance'));
+/*      const entryChanges = hasUnsavedAttributes(controller.get('model.entryInstance'));
       const contactChanges = hasUnsavedAttributes(controller.get('model.contactInfoInstance'));
       const locationChanges = hasUnsavedAttributes(controller.get('model.locationInstance'));
       const annotationChanges = hasUnsavedAttributes(controller.get('model.annotationInstance'));
@@ -28,7 +28,7 @@ export default Ember.Route.extend(RouteHelper, {
         const alertData = {title: 'Ungespeicherte Informationen', description: 'Die eingetragenen Informationen wurden nicht gespeichert', isError: true, autoHide: false};
         this.EventBus.publish('showAlert', alertData);
         transition.abort();
-      }
+      }*/
 
       controller.get('model.entryInstance').rollbackAttributes();
       controller.get('model.contactInfoInstance').rollbackAttributes();
