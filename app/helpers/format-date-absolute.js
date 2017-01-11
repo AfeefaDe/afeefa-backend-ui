7200b49c741324babb823a4e0c2016826226a7fc
Jens Struwe <jens@struwe.net>
2017-01-11 14:58:00 +0100

    add leading zeros (01.04.2015 04:40) in date strings

--- app/helpers/format-date-absolute.js
+++ app/helpers/format-date-absolute.js
@@ -1,13 +1,22 @@
 import Ember from 'ember';
 
+function addZero(i) {
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
+}
+
 export function formatDateAbsolute(params/*, hash*/) {
   if(params && params[0]) {
     let date = params[0];
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
+    var month = addZero(date.getMonth() + 1);
+    var day = addZero(date.getDate());
     var year = date.getFullYear();
+    const hour = addZero(date.getHours());
+    const minutes = addZero(date.getMinutes());
     /*jshint newcap: false */
-    return `${day}.${month}.${year}`;
+    return `${day}.${month}.${year} um ${hour}:${minutes} Uhr`;
   }
   else return params;
 }
