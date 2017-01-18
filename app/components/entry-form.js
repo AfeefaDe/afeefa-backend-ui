5c4fd106a909c6ee12cf055e574d1abf42f580e7
hagen <you@example.com>
2017-01-18 17:37:24 +0100

    add to entry-form end date, end time and a button to show or delete end time

--- app/components/entry-form.js
+++ app/components/entry-form.js
@@ -11,34 +11,61 @@ export default Ember.Component.extend(FormatReasonErrorMessage, {
     const entry = this.get('model.entryInstance');
     return entry.date_start || entry.date_start === null;
   }),
+  // start date and start time variables
   dateStartObject: '',
   hasStartTime: '',
   showStartTime: '',
   startTimeIconState: '',
   startTimeButtonColor: '',
+  // end date and end time variables
+  dateEndObject: '',
+  hasEndTime: '',
+  showEndTime: '',
+  endTimeIconState: '',
+  endTimeButtonColor: '',
   didReceiveAttrs() {
     this._super(...arguments);
     const dateStart = this.get('model.entryInstance.date_start');
     if(dateStart) {
       this.set('dateStartObject', dateStart);
     } else {
-      // date object is needed to set values
       this.set('dateStartObject', new Date());
     }
-    //const hasStartTime = this.get('model.entryInstance.hasStartTime'); // TODO
-    const hasStartTime = true;
-    if(hasStartTime) {
+    const dateEnd = this.get('model.entryInstance.date_end');
+    if(dateEnd) {
+      this.set('dateEndObject', dateEnd);
+    } else {
+      this.set('dateEndObject', new Date());
+    }
+
+    const has_time_start = this.get('model.entryInstance.has_time_start');
+    //const has_time_start = true;
+    if(has_time_start) {
       this.set('hasStartTime', true);
       this.set('showStartTime', true);
-      this.set('startTimeIconState', 'alarm_off');
+      this.set('startTimeIconState', 'delete_forever');
       this.set('startTimeButtonColor', 'red');
     } else {
       this.set('hasStartTime', false);
       this.set('showStartTime', false);
       this.set('startTimeIconState', 'alarm');
     }
+
+    const has_time_end = this.get('model.entryInstance.has_time_end');
+    //const has_time_end = true;
+    if(has_time_end) {
+      this.set('hasEndTime', true);
+      this.set('showEndTime', true);
+      this.set('endTimeIconState', 'delete_forever');
+      this.set('endTimeButtonColor', 'red');
+    } else {
+      this.set('hasEndTime', false);
+      this.set('showEndTime', false);
+      this.set('endTimeIconState', 'alarm');
+    }
   },
 	actions: {
+    // start date and start time
     updateStartDate: function(newStartDate) {
       const day = newStartDate[0].getDate();
       const month = newStartDate[0].getMonth();
@@ -54,30 +81,58 @@ export default Ember.Component.extend(FormatReasonErrorMessage, {
 
       this.get('dateStartObject').setHours(hours);
       this.get('dateStartObject').setMinutes(minutes);
+      this.set('hasStartTime', true);
     },
     toggleStartTimeElement: function() {
       if(this.get('showStartTime')) {
-        // hide start time and delete time values
+        // hide start time and delete start time values
         this.set('startTimeIconState', 'alarm');
         this.set('startTimeButtonColor', '');
         this.get('dateStartObject').setHours(0);
         this.get('dateStartObject').setMinutes(0);
         this.get('dateStartObject').setMilliseconds(0)
+        this.set('hasStartTime', false);
       } else {
         // show start time
-        this.set('startTimeIconState', 'alarm_off');
+        this.set('startTimeIconState', 'delete_forever');
         this.set('startTimeButtonColor', 'red');
       }
       this.toggleProperty('showStartTime');
     },
+    //end date and end time
+    updateEndDate: function(newEndDate) {
+      const day = newEndDate[0].getDate();
+      const month = newEndDate[0].getMonth();
+      const year = newEndDate[0].getFullYear();
 
+      this.get('dateEndObject').setDate(day);
+      this.get('dateEndObject').setMonth(month);
+      this.get('dateEndObject').setFullYear(year);
+    },
+    updateEndTime: function(newEndTime) {
+      const hours = newEndTime[0].getHours();
+      const minutes = newEndTime[0].getMinutes();
 
-    // toggleFlatpickrStatus() {
-    //   this.get('flatpickr').close();
-    // },
-    // clearFlatpickrDate() {
-    //   this.get('flatpickr').clear();
-    // },
+      this.get('dateEndObject').setHours(hours);
+      this.get('dateEndObject').setMinutes(minutes);
+      this.set('hasEndTime', true);
+    },
+    toggleEndTimeElement: function() {
+      if(this.get('showEndTime')) {
+        // hide end time and delete end time values
+        this.set('endTimeIconState', 'alarm');
+        this.set('endTimeButtonColor', '');
+        this.get('dateEndObject').setHours(0);
+        this.get('dateEndObject').setMinutes(0);
+        this.get('dateEndObject').setMilliseconds(0)
+        this.set('hasEndTime', false);
+      } else {
+        // show end time
+        this.set('endTimeIconState', 'delete_forever');
+        this.set('endTimeButtonColor', 'red');
+      }
+      this.toggleProperty('showEndTime');
+    },
     /*
      * Save Entry with meta models
      */
@@ -85,9 +140,15 @@ export default Ember.Component.extend(FormatReasonErrorMessage, {
       let entry = this.get('model.entryInstance');
       //determine new or edit mode
       const isEditMode = entry.get('id');
-      // save date in entry model
+      // save start date in entry model
       if(entry.date_start || entry.date_start === null) {
           this.set('model.entryInstance.date_start', this.get('dateStartObject'));
+          this.set('model.entryInstance.has_time_start', this.get('hasStartTime'));
+      }
+      // save end date in entry model
+      if(entry.date_end || entry.date_end === null) {
+          this.set('model.entryInstance.date_end', this.get('dateEndObject'));
+          this.set('model.entryInstance.has_time_end', this.get('hasEndTime'));
       }
 
       entry.get('contactInfos').pushObject(this.get('model.contactInfoInstance'));
