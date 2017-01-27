bd08448aa08b5261c3baa4a3d49f55eee3cb4bf3
friedrichweise <hi@weise.io>
2017-01-27 14:10:07 +0100

    add navigation-sidebar footer

--- app/components/navigation-sidebar.js
+++ app/components/navigation-sidebar.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  session: Ember.inject.service('session'),
+  actions: {
+    invalidateSession() {
+      this.get('session').invalidate();
+    }
+  }
 });
