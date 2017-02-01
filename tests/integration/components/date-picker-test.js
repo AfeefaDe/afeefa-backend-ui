756bb24034b8ce5bd300ae9ca47117dc31c66620
hagen <you@example.com>
2017-02-01 15:46:18 +0100

    begin with date-picker test

--- tests/integration/components/date-picker-test.js
+++ tests/integration/components/date-picker-test.js
@@ -19,5 +19,23 @@ test('it renders', function(assert) {
     this.render(hbs`{{date-picker model=model}}`);
 
     assert.equal(this.$('button').eq(0).text().trim(), 'event');
+    assert.equal(this.$('button').length, 4);
+  });
+
+});
+
+test('if start date is set in the model, it is shown', function(assert) {
+  Ember.run(() => {
+    const store = this.get('store');
+    this.set('model', fixtures.setupEvent(store));
+
+    const currentDay = new Date(2017, 1, 28);
+    this.set('model.entryInstance.date_start', currentDay);
+    this.render(hbs`{{date-picker model=model}}`);
+    assert.equal(this.$('#startDate').val(), '28.02.2017');
+
+    // console.log(this.$().html());
+    // console.log(this.$('#startDate').html());
+    // console.log(this.get('model.entryInstance'));
   });
 });
