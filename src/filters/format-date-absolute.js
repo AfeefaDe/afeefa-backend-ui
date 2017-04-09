787a7e8f8a9f0d55d59ec73b3f6c4d8ca9678fef
friedrichweise <hi@weise.io>
2017-04-09 14:35:02 +0200

    bugfix: check for valid date before converting date format

--- src/filters/format-date-absolute.js
+++ src/filters/format-date-absolute.js
@@ -1,9 +1,14 @@
+import moment from 'moment'
 export default function (date) {
   function addZero (i) {
     if (i < 10) {
       i = '0' + i
     }
     return i
+  }
+
+  if (!moment(date).isValid()) {
+    return 'Invalid Date'
   }
 
   var month = addZero(date.getMonth() + 1)
