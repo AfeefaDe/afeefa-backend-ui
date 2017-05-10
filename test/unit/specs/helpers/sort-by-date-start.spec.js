f79094e535916160d88dff44676710ed1109cc10
friedrichweise <hi@weise.io>
2017-05-10 15:58:58 +0200

    remove blank lines to solve jslint test

--- test/unit/specs/helpers/sort-by-date-start.spec.js
+++ test/unit/specs/helpers/sort-by-date-start.spec.js
@@ -2,7 +2,6 @@ import sortByDateStart from '@/helpers/sort-by-date-start'
 
 describe.only('Helpers - sort-by-date-start', () => {
   it('should sort ASC by default', () => {
-
     const a = { date_start: new Date('2011-04-11') }
     const b = { date_start: new Date('2011-04-12') }
     const c = { date_start: new Date('2011-04-13') }
@@ -13,7 +12,6 @@ describe.only('Helpers - sort-by-date-start', () => {
 
     expect(sortedItems).to.deep.equal([d, a, b, c])
   })
-
 
   it('should sort DESC if given', () => {
     const a = { date_start: new Date('2011-04-11') }
