c8109b045720844c4b7ee9d15319cf049615116c
hagenbeelitz <you@example.com>
2017-06-30 18:19:13 +0200

    init search-sidebar component

--- src/components/routes/Search.vue
+++ src/components/routes/Search.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="row">
-    <div class="col s12 m12">
-      <div class="mainCard">
-        <div class="mainCard__header">
-          <h2 class="mainCard__headerTitle">Search </h2>
+  <div class="search-container">
+
+    <div class="row search-field">
+      <div class="col s12 m12">
+        <div class="mainCard">
+          <div class="mainCard__header">
+            <h2 class="mainCard__headerTitle">Search </h2>
+          </div>
+          <div>
+            <search-items :modifyRoute="true"></search-items>
+          </div>
         </div>
-        <div>
-          <search-items :modifyRoute="true"></search-items>
-        </div>
+      </div>
+    </div>
+
+    <div class="mainCard search-sidebar">
+      <div>
+        <search-sidebar-element> </search-sidebar-element>
+      </div>
+      <div>
+        <fieldset>
+          <legend>Zeige</legend>
+          <div class="switch-toggle switch-candy">
+            <input id="orgas" name="view" type="radio">
+            <label for="orgas" onclick="">Orgas</label>
+
+            <input id="both" name="view" type="radio" checked>
+            <label for="both" onclick="">Beides</label>
+
+            <input id="events" name="view" type="radio">
+            <label for="events" onclick="">Events</label>
+          </div>
+        </fieldset>
       </div>
     </div>
   </div>
@@ -16,7 +40,25 @@
 
 <script>
 import SearchItems from '@/components/SearchItems'
+import SearchSidebarElement from '@/components/SearchSidebarElement'
+
 export default {
-  components: { SearchItems }
+  components: { SearchItems, SearchSidebarElement }
 }
 </script>
+
+<style lang="scss" scoped>
+.search-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+.search-field {
+  flex-grow: 3;
+}
+.search-sidebar {
+  flex-grow: 1;
+  background-color: lightblue;
+}
+
+</style>
