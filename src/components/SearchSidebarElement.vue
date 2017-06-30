3b9f771263b9595988044928079c40c9d65a4737
hagenbeelitz <you@example.com>
2017-06-30 18:19:13 +0200

    init search-sidebar component

---
+++ src/components/SearchSidebarElement.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sidebar-element">
+    <input class="filled-in" id="titel" type="checkbox" @click="" v-model="titel">
+    <label for="titel">Titel</label>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+@import "~variables";
+
+.sidebar-element {
+  width: auto;
+  height: 10em;
+  background-color: lightblue;
+}
+
+</style>
