77617a2be67b1b8f5e211c69327d1b4a527e4584
friedrichweise <account@weise.io>
2018-01-10 11:22:44 +0100

    #389 add mixin file for frontend link generation

---
+++ src/components/mixins/GenerateFrontendLinkMixin.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  computed: {
+    /*
+     * generates the frontend link from the current user context
+     */
+    frontendURL () {
+      let url = '//'
+      if (this.currentUser && this.currentUser.area && this.currentUser.area.toLowerCase() !== 'dresden') {
+        url += this.currentUser.area + '.'
+      }
+      return url + process.env.FRONTEND_URL
+    }
+  }
+}
+</script>
