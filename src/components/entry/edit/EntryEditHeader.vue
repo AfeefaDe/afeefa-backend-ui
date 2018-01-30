a9c38856bb87810905fb188edbd74ea29ecfa4c2
Jens Struwe <jens@struwe.net>
2018-01-30 22:50:49 +0100

    adjust entry header styles (edit == show)

--- src/components/entry/edit/EntryEditHeader.vue
+++ src/components/entry/edit/EntryEditHeader.vue
@@ -1,13 +1,16 @@
 <template>
-  <div v-bind:class="['mainCard__header', 'mainCard__headerCategories', categoryClass]" v-if="item">
+  <div :class="['mainCard__header', 'mainCard__headerCategories', categoryClass]" v-if="item">
+    <a href="" @click.prevent="goBack"><i class="material-icons go-back">chevron_left</i></a>
+
     <div class="mainCard__headerTitle">
-      <h2 class="mainCard__headerTitle"> {{item.title || defaultTitle }}</h2>
+      <h2 class="mainCard__headerTitleHeading"> {{item.title || defaultTitle }}</h2>
       <span v-if="item.parent_orga" class="mainCard__headerSubtitle">
         <router-link :to="{name: item.parent_orga.type + '.show', params: {id: item.parent_orga.id}}">
           <u>{{ item.parent_orga.title }}</u>
         </router-link>
       </span>
     </div>
+
     <a href="" @click.prevent="cancel" class="mainCard__headerAction"><i class="material-icons">cancel</i></a>
   </div>
 </template>
@@ -37,6 +40,10 @@ export default {
   },
 
   methods: {
+    goBack () {
+      this.$router.go(-1)
+    },
+
     cancel () {
       if (this.item.id) {
         this.$router.push({name: this.routeName + '.show', params: {id: this.item.id}})
