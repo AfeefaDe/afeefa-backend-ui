14fc7616e3b2c1619f0bbed40544bff275a9283b
Jens Struwe <jens@struwe.net>
2018-03-07 13:52:54 +0100

    fix actor relations resource url

--- src/resources/relations/ActorRelations.js
+++ src/resources/relations/ActorRelations.js
@@ -1,8 +1,8 @@
 import RelationResource from 'uidata/resource/RelationResource'
 
 export default class ActorRelationsResource extends RelationResource {
-  init () {
-    this.url = 'orgas{/id}/actor_relations'
+  getUrl () {
+    return 'orgas{/id}/actor_relations'
   }
 
   getItemJson (json) {
