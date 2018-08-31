2ac89bdfabaa2b2b7c344c2b251613e9d0206de2
Jens Struwe <jens@struwe.net>
2018-08-31 22:17:34 +0200

    #479 cancel new actor.offer/actor.event returs back to actor

--- src/components/entry/EntryHeader.vue
+++ src/components/entry/EntryHeader.vue
@@ -14,17 +14,17 @@
     <div slot="buttons" v-if="entry">
       <slot name="buttons" />
 
-      <router-link :to="{name: this.routeName + '.show', params: {id: entry.id}}" class="btn gray btn-medium" v-if="isEdit && entry.id">
+      <router-link :to="{name: routeName + '.show', params: {id: entry.id}}" class="btn gray btn-medium" v-if="isEdit && entry.id">
         <i class="material-icons left">cancel</i>
         {{ $t('buttons.cancel') }}
       </router-link>
 
-      <router-link :to="{name: this.routeName + '.list'}" class="btn gray btn-medium" v-else-if="isEdit && !entry.id">
+      <router-link :to="cancelAddUrl" class="btn gray btn-medium" v-else-if="isEdit && !entry.id">
         <i class="material-icons left">cancel</i>
         {{ $t('buttons.cancel') }}
       </router-link>
 
-      <router-link :to="{name: this.routeName + '.edit', params: {id: entry.id}}" class="btn green btn-medium" v-if="!isEdit">
+      <router-link :to="{name: routeName + '.edit', params: {id: entry.id}}" class="btn green btn-medium" v-if="!isEdit">
         <i class="material-icons left">mode_edit</i>
         {{ $t('buttons.edit') }}
       </router-link>
@@ -48,6 +48,14 @@ export default {
   computed: {
     owners () {
       return this.entryOwners(this.entry)
+    },
+
+    cancelAddUrl () {
+      if (this.$route.query.actorId) {
+        return {name: 'orgas.show', params: {id: this.$route.query.actorId}}
+      } else {
+        return {name: this.routeName + '.list'}
+      }
     }
   },
 
